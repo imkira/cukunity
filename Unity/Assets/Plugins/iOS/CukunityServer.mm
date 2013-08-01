617f4c95878687db88f223014a28e7c4a5de73c5
Mário Freitas <imkira@gmail.com>
2013-08-01 21:42:08 +0900

    fixed: UITouch ivars are now unavailable from iOS6 SDK headers

--- Unity/Assets/Plugins/iOS/CukunityServer.mm
+++ Unity/Assets/Plugins/iOS/CukunityServer.mm
@@ -40,6 +40,38 @@
 
 @end
 
+#ifdef __IPHONE_6_0
+@interface UITouch ()
+{
+  NSTimeInterval _timestamp;
+  UITouchPhase _phase;
+  UITouchPhase _savedPhase;
+  NSUInteger _tapCount;
+
+  UIWindow *_window;
+  UIView *_view;
+  UIView *_gestureView;
+  UIView *_warpedIntoView;
+  NSMutableArray *_gestureRecognizers;
+  NSMutableArray *_forwardingRecord;
+
+  CGPoint _locationInWindow;
+  CGPoint _previousLocationInWindow;
+  UInt8 _pathIndex;
+  UInt8 _pathIdentity;
+  float _pathMajorRadius;
+  struct
+  {
+    unsigned int _firstTouchForView:1;
+    unsigned int _isTap:1;
+    unsigned int _isDelayed:1;
+    unsigned int _sentTouchesEnded:1;
+    unsigned int _abandonForwardingRecord:1;
+  } _touchFlags;
+}
+@end
+#endif
+
 //
 // UIEvent (Synthesize)
 //
